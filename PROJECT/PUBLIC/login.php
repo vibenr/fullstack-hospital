c7b9b2d3d10e71fee4eb1686da0e53be0d2d5bf3
yug <yugkhokhar18@gmail.com>
2021-08-03 19:10:50 +0530

    updated login and css

--- PROJECT/PUBLIC/login.php
+++ PROJECT/PUBLIC/login.php
@@ -1,36 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-
-    <section id="login">
-
-        <form action="">
-            <div>
-                <label for="email">Email</label>
-                <input type="text"> 
-            </div>
-
-            <div>
-                <label for="password">Password</label>
-                <input type="password"> 
-            </div>
-
-           <button type="submit">Submit</button>
-
-
-
         
-        </form>
+    <link href="stylesheets/login.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>LOGIN PAGE</title>
+    </head>
+<body>
+ <header>
+
+ </header>
+
+    <div id="container">
+       <header>MEMBER LOGIN</header>
+       <form method="post" action="">
+          <fieldset>
+             <br/>
+             <input type="text" name="username" id="username" placeholder="Username"  required autofocus value="">
+             <br/><br/>
+             <input type="password" name="password" id="password" placeholder="Password"  required  value="">
+             <br/><br/>
+             <label for="submit"></label>
+             <input type="submit" name="submit" id="submit" value="LOGIN">
+             <p class="text-center md">Don't have an account? <a href="register.php">Sign up here!</a></p>
+          </fieldset>
+       </form>
+    </div>
 
 
+   <footer>
 
-    </section>
-    
-    </body>
-</html>
+   </footer>
+ </body>
+ </html>
