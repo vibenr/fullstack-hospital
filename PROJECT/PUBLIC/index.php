0d88624a8b1e9da79f687e052a474d154273051f
vaibhav <vaibhavchauhan2410@gmail.com>
2021-08-04 17:43:26 +0530

    Successfully added index page which is now linked with log in and register

--- PROJECT/PUBLIC/index.php
+++ PROJECT/PUBLIC/index.php
@@ -1,11 +1,325 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Infinity Hospital Centre</title>
-    </head>
-    <body>
-        
-        
-    </body>
+<html lang="">
+<head>
+<title>INFINITY HEALTHCARE</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="stylesheets/index.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+</head>
+<body id="top">
+  >
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<!-- <div class"nav-bar">
+  <div id="topbar" class="hoc clear">  -->
+    <!-- ################################################################################################ -->
+    <!-- <div class="fl_left">
+      <ul>
+        <img src="images/demo/logo.jpg" alt="" class = "logo">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Register</a></li>
+      </ul>
+    </div> -->
+    <!-- ################################################################################################ --> 
+  <!-- </div>
+</div> -->
+<nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+
+  <div class="navbar-collapse collapse" id="navbarcollapse">
+     <ul class="nav nav-tabs ">  
+       <li ><a href="index.php">HOME</a></li>
+       <li ><a href="login.php">LOGIN</a></li>
+       <li ><a href="register.php">REGISTER </a></li>
+     </ul>
+
+  
+
+
+</div>
+
+</div>
+</nav>
+
+
+
+
+
+
+
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<!--  -->
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('images/01.jpg');">
+  <article id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <h3 class="heading">INFINITY HEALTH CENTER</p>
+    <footer><a class="btn" href="#">Come And Meet Us To Know More</a></footer>
+    <!-- ################################################################################################ --> 
+  </article>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body --> 
+    <!-- ################################################################################################ -->
+    <ul class="nospace group">
+      <li class="one_third first">
+        <article><i >
+          <img src="images/logo1.png" alt="">
+        </i>
+          <h6 class="heading font-x1">Qualified Doctors</h6>
+          <p class="btmspace-30">We have best qualified doctors in whole country and one of the best in the world.. [&hellip;]</p>
+          <footer><a class="btn" href="#">Read More &rsaquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><i >
+          <img src="images/logo2.png" alt="">
+        </i>
+          <h6 class="heading font-x1">Medical Treatment</h6>
+          <p class="btmspace-30">We are providing the best medical treatment in the world. and with the benchmark technologies. [&hellip;]</p>
+          <footer><a class="btn" href="#">Read More &rsaquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><i >
+          <img src="images/logo3.png" alt="">
+        </i>
+          <h6 class="heading font-x1">Emergency Help</h6>
+          <p class="btmspace-30">A small river named dudent flows by their place supplies it with the the regulations. [&hellip;]</p>
+          <footer><a class="btn" href="#">Read More &rsaquo;</a></footer>
+        </article>
+      </li>
+    </ul>
+    <!-- ################################################################################################ --> 
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('images/02.jpg');">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h3 class="heading">We are Here For Your Care</h3>
+    </div>
+    <div class="group">
+      <div class="one_half first">
+        <p>Far Far away , behind the word mountains , far from the countries Vokalia and Consonanita , There live the blind texts , Separated they live in bookmarksgrove. We provides you the best facilites in india.. at very reliable source and profitable prices..</p>
+      </div>
+    </div>
+    <!-- ################################################################################################ --> 
+  </section>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<br>
+<br>
+<br>
+<div class="wrapper bgded overlay" style="background-image:url('images/03.jpg');">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">Our Team</h6>
+    </div>
+    <div class="group team">
+      <figure class="one_quarter first"><a href="#"><img src="images/1.jpg" alt=""></a>
+        <figcaption>
+          <h6 class="heading">Mrs.sonal patel</h6>
+          <em>Delhi</em>
+          <footer>
+            <ul class="faico clear">
+              <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+              <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+            </ul>
+          </footer>
+        </figcaption>
+      </figure>
+      <figure class="one_quarter"><a href="#"><img src="images/2.jpg" alt=""></a>
+        <figcaption>
+          <h6 class="heading">Mr. Aman Dhattarwal</h6>
+          <em>Ahmedabad</em>
+          <footer>
+            <ul class="faico clear">
+              <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+              <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+            </ul>
+          </footer>
+        </figcaption>
+      </figure>
+      <figure class="one_quarter"><a href="#"><img src="images/3.jpg" alt=""></a>
+        <figcaption>
+          <h6 class="heading">Ms.Heena Shah</h6>
+          <em>Mumbai</em>
+          <footer>
+            <ul class="faico clear">
+              <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+              <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+            </ul>
+          </footer>
+        </figcaption>
+      </figure>
+      <figure class="one_quarter"><a href="#"><img src="images/4.jpg" alt=""></a>
+        <figcaption>
+          <h6 class="heading">Ms. Shanti Chauhan</h6>
+          <em>Mumbai</em>
+          <footer>
+            <ul class="faico clear">
+              <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+              <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+            </ul>
+          </footer>
+        </figcaption>
+      </figure>
+      <!-- <figure class="one_quarter"><a href="#"><img src="images/demo/4.jpg" alt=""></a>
+        <figcaption>
+          <h6 class="heading">F. Name</h6>
+          <em>Consequat ex arcu</em>
+          <footer>
+            <ul class="faico clear">
+              <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+              <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+            </ul>
+          </footer>
+        </figcaption>
+      </figure> -->
+    </div>
+    <!-- ################################################################################################ -->
+    <div class="clear"></div>
+  </section>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <figure class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <!-- <ul class="nospace group logos">
+      <li class="one_quarter first"><a href="#"><img src="images/demo/222x100.png" alt=""></a></li>
+      <li class="one_quarter"><a href="#"><img src="images/demo/222x100.png" alt=""></a></li>
+      <li class="one_quarter"><a href="#"><img src="images/demo/222x100.png" alt=""></a></li>
+      <li class="one_quarter"><a href="#"><img src="images/demo/222x100.png" alt=""></a></li>
+    </ul> -->
+    <!-- ################################################################################################ --> 
+  </figure>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_quarter first ">
+      <h2 class="heading">About Us</h2>
+      <p>
+        The Developers Team of LDCE
+        <ul>
+          <li>
+            Vaibhav Chauhan(Front-End)
+          </li><br>
+          <li>
+            Yug Khokhar(Front-End)
+          </li><br>
+          <li>
+            Jaimin Chokhawala(Back-End)
+          </li><br>
+        </ul>
+      </p>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading">Technologies</h6>
+      <ul class="nospace linklist">
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JAVASCRIPT</li>
+        <li>BOOTSTRAP</li>
+        <li>PHP</li>
+        <li>MY SQL</li>
+      </ul>
+    </div>
+    <div class="one_quarter  ">
+      <h6 class="heading">Contact Us</h6>
+      <ul class="nospace linklist">
+        <li>vaibhavchauhan2410@gmail.com</li>
+        <li>9913180135</li>
+        <li>9016773869</li>
+        <li>8866229813</li>
+      </ul>
+    </div>
+    <div class="one_quarter  ">
+      <h6 class="heading">Facilities</h6>
+      <ul class="nospace linklist">
+        <li>Orthopedic</li>
+        <li>Cardiographic</li>
+        <li>Psychologist</li>
+        <li>Gynochologist</li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ --> 
+  </footer>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">Demo-2021</a></p>
+    <p class="fl_right">Developed by Team developers LDCE</p>
+    <!-- ################################################################################################ --> 
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a> 
+<!-- JAVASCRIPTS --> 
+<!-- <script src="layout/scripts/jquery.min.js"></script> -->
+<!-- <script src="layout/scripts/jquery.backtotop.js"></script> -->
+<!-- <script src="layout/scripts/jquery.mobilemenu.js"></script> -->
+</body>
 </html>
